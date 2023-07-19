0f6f6bc6137d65e5206aa890a7a62ad3565baa05
Momin Tahir <momintahir7@gmail.com>
2023-07-19 19:55:07 +0500

    nutrient goal screen

--- app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -23,6 +23,7 @@ import com.plcoding.onboarding_presentation.age.AgeScreen
 import com.plcoding.onboarding_presentation.gender.GenderScreen
 import com.plcoding.onboarding_presentation.goal.GoalScreen
 import com.plcoding.onboarding_presentation.height.HeightScreen
+import com.plcoding.onboarding_presentation.nutrient_goal.NutrientGoalScreen
 import com.plcoding.onboarding_presentation.weight.WeightScreen
 import com.plcoding.onboarding_presentation.welcome.WelcomeScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -61,7 +62,7 @@ class MainActivity : ComponentActivity() {
                         WeightScreen(scaffoldState = scaffoldState, onNavigate =navController::navigate )
                         }
                         composable(Route.NUTRIENT_GOAL) {
-
+                        NutrientGoalScreen(scaffoldState = scaffoldState, onNavigate =navController::navigate )
                         }
                         composable(Route.ACTIVITY) {
                             ActivityScreen(onNavigate = navController::navigate)
